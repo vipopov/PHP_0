0dbeff128105470cae31d8f14a64c8bf60bbf236
Volodymyr <58085784+vipopov@users.noreply.github.com>
2019-11-24 19:33:26 +0200

    add proxy

--- index.php
+++ index.php
@@ -1,2 +1,43 @@
 <?php
-// code
+// code from https://thisinterestsme.com/php-curl-proxy/
+//The URL you want to send a cURL proxy request to.
+$url = 'http://php.net';
+
+//The IP address of the proxy you want to send
+//your request through.
+$proxyIP = '1.2.3.4';
+
+//The port that the proxy is listening on.
+$proxyPort = '1129';
+
+//The username for authenticating with the proxy.
+$proxyUsername = 'myusername';
+
+//The password for authenticating with the proxy.
+$proxyPassword = 'mypassword';
+
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL , 1);
+
+//Set the proxy IP.
+curl_setopt($ch, CURLOPT_PROXY, $proxyIP);
+
+//Set the port.
+curl_setopt($ch, CURLOPT_PROXYPORT, $proxyPort);
+
+//Specify the username and password.
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, "$proxyUsername:$proxyPassword");
+
+//Execute the request.
+$output = curl_exec($ch);
+
+//Check for errors.
+if(curl_errno($ch)){
+    throw new Exception(curl_error($ch));
+}
+
+//Print the output.
+echo $output;
